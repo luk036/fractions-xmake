de063130e98fec2e6380d8c08c4e4b2e30c39ad1
Wai-Shing Luk <luk036@gmail.com>
2022-08-07 17:13:07 +0800

    fix compilation error

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -6,6 +6,6 @@
 using fun::Fraction;
 
 TEST_CASE("it works") {
-    auto p = Fraction{30, 40};
-    CHECK_EQ(p, Fraction{3, 4});
+    auto p = Fraction<int>{30, 40};
+    CHECK_EQ(p, Fraction<int>{3, 4});
 }
